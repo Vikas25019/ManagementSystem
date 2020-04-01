a69e186546ae600ae4ae5087706f0c0390da27b0
Vikas25019 <35371508+Vikas25019@users.noreply.github.com>
2020-04-01 07:38:31 -0500

    Add files via upload

--- src/main/java/dao/IDaoInterface.java
+++ src/main/java/dao/IDaoInterface.java
@@ -3,12 +3,13 @@ package dao;
 import java.net.UnknownHostException;
 import java.sql.SQLException;
 
+import java.util.List;
 import java.util.Map;
 
 public interface IDaoInterface<T, U> {
     int create(T t, U u, Map<String, String> data) throws SQLException, ClassNotFoundException, UnknownHostException;
 
-    void retrieve(T t, U u, Map<String, String> data) throws SQLException, ClassNotFoundException, UnknownHostException;
+    Map<String, String> retrieve(T t, U u, Map<String, String> data) throws SQLException, ClassNotFoundException, UnknownHostException;
 
     int update(T t, U u, Map<String, String> data ,String columnName) throws SQLException, ClassNotFoundException, UnknownHostException;
 
@@ -16,4 +17,5 @@ public interface IDaoInterface<T, U> {
 
     <P,E> boolean isIdPresent(P p, E e, Map<String, String> data) throws SQLException, ClassNotFoundException, UnknownHostException;
 
+    List<List<String>> retrieveAll(T t,U u) throws SQLException, ClassNotFoundException;
 }
